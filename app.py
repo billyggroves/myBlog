f3876d14e3a53c85f56cfe3c72f39905e69693ac
Bill Groves <billyggroves@gmail.com>
2018-03-01 23:13:53 -0500

    Add Portfolio projects

--- app.py
+++ app.py
@@ -12,33 +12,33 @@ ALLOWED_EXTENSIONS = set(['jpg', 'jpeg'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-# app.config['SQLALCHEMY_DATABASE_URI'] = ''
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = ''
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-# class Blog(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     imgHead = db.Column(db.String(80), unique=False, nullable=True)
-#     title = db.Column(db.String(80), unique=True, nullable=False)
-#     subtitle1 = db.Column(db.String(80), unique=False, nullable=True)
-#     intro = db.Column(db.Text, unique=True, nullable=False)
-#     subtitle2 = db.Column(db.String(80), unique=False, nullable=True)
-#     body = db.Column(db.Text, unique=True, nullable=True)
-#     subtitle3 = db.Column(db.String(80), unique=False, nullable=True)
-#     conclusion = db.Column(db.Text, unique=True, nullable=True)
-#     date = db.Column(db.DateTime, unique=True, nullable=False)
+class Blog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    imgHead = db.Column(db.String(80), unique=False, nullable=True)
+    title = db.Column(db.String(80), unique=True, nullable=False)
+    subtitle1 = db.Column(db.String(80), unique=False, nullable=True)
+    intro = db.Column(db.Text, unique=True, nullable=False)
+    subtitle2 = db.Column(db.String(80), unique=False, nullable=True)
+    body = db.Column(db.Text, unique=True, nullable=True)
+    subtitle3 = db.Column(db.String(80), unique=False, nullable=True)
+    conclusion = db.Column(db.Text, unique=True, nullable=True)
+    date = db.Column(db.DateTime, unique=True, nullable=False)
 
-#     def __init__(self, id, imgHead, title, subtitle1, intro, subtitle2, body, subtitle3, conclusion, date):
-#         self.id = id
-#         self.imgHead = imgHead
-#         self.title = title
-#         self.subtitle1 = subtitle1
-#         self.intro = intro
-#         self.subtitle2 = subtitle2
-#         self.body = body
-#         self.subtitle3 = subtitle3
-#         self.conclusion = conclusion
-#         self.date = date
+    def __init__(self, id, imgHead, title, subtitle1, intro, subtitle2, body, subtitle3, conclusion, date):
+        self.id = id
+        self.imgHead = imgHead
+        self.title = title
+        self.subtitle1 = subtitle1
+        self.intro = intro
+        self.subtitle2 = subtitle2
+        self.body = body
+        self.subtitle3 = subtitle3
+        self.conclusion = conclusion
+        self.date = date
 
 
 JSGlue(app)
@@ -59,6 +59,18 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+@app.route("/prospectRoulette")
+def prospectRoulette():
+    return render_template("prospectRoulette.html")
+
+@app.route("/myBlog")
+def myBlog():
+    return render_template("myBlog.html")
+
+@app.route("/scoreCard")
+def scoreCard():
+    return render_template("scoreCard.html")
 
 @app.route("/post", methods=['GET', 'POST'])
 def post():
