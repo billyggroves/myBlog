1f4590cd61c69abc4cee9e7aad57ae95e60ca80a
Bill Groves <billyggroves@gmail.com>
2018-03-02 17:49:12 -0500

    Moving to Desktop

--- app.py
+++ app.py
@@ -12,33 +12,33 @@ ALLOWED_EXTENSIONS = set(['jpg', 'jpeg'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['SQLALCHEMY_DATABASE_URI'] = ''
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+# app.config['SQLALCHEMY_DATABASE_URI'] = ''
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db = SQLAlchemy(app)
 
-class Blog(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    imgHead = db.Column(db.String(80), unique=False, nullable=True)
-    title = db.Column(db.String(80), unique=True, nullable=False)
-    subtitle1 = db.Column(db.String(80), unique=False, nullable=True)
-    intro = db.Column(db.Text, unique=True, nullable=False)
-    subtitle2 = db.Column(db.String(80), unique=False, nullable=True)
-    body = db.Column(db.Text, unique=True, nullable=True)
-    subtitle3 = db.Column(db.String(80), unique=False, nullable=True)
-    conclusion = db.Column(db.Text, unique=True, nullable=True)
-    date = db.Column(db.DateTime, unique=True, nullable=False)
+# class Blog(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     imgHead = db.Column(db.String(80), unique=False, nullable=True)
+#     title = db.Column(db.String(80), unique=True, nullable=False)
+#     subtitle1 = db.Column(db.String(80), unique=False, nullable=True)
+#     intro = db.Column(db.Text, unique=True, nullable=False)
+#     subtitle2 = db.Column(db.String(80), unique=False, nullable=True)
+#     body = db.Column(db.Text, unique=True, nullable=True)
+#     subtitle3 = db.Column(db.String(80), unique=False, nullable=True)
+#     conclusion = db.Column(db.Text, unique=True, nullable=True)
+#     date = db.Column(db.DateTime, unique=True, nullable=False)
 
-    def __init__(self, id, imgHead, title, subtitle1, intro, subtitle2, body, subtitle3, conclusion, date):
-        self.id = id
-        self.imgHead = imgHead
-        self.title = title
-        self.subtitle1 = subtitle1
-        self.intro = intro
-        self.subtitle2 = subtitle2
-        self.body = body
-        self.subtitle3 = subtitle3
-        self.conclusion = conclusion
-        self.date = date
+#     def __init__(self, id, imgHead, title, subtitle1, intro, subtitle2, body, subtitle3, conclusion, date):
+#         self.id = id
+#         self.imgHead = imgHead
+#         self.title = title
+#         self.subtitle1 = subtitle1
+#         self.intro = intro
+#         self.subtitle2 = subtitle2
+#         self.body = body
+#         self.subtitle3 = subtitle3
+#         self.conclusion = conclusion
+#         self.date = date
 
 
 JSGlue(app)
